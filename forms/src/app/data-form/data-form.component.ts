f48e8fdc5d1b2a009a408c4b231abfabd3728ea2
deyvison uchoa <deyvisonuchoa@hotmail.com>
2020-10-29 23:25:54 -0300

    aula 90 formBuilder

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
@@ -10,9 +10,20 @@ export class DataFormComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+
+    /*this.form = new FormGroup({
+      nome: new FormControl(null),
+      email: new FormControl(null)
+    });*/
+
+    this.form = this.formBuilder.group({
+      nome: [null],
+      email: [null]
+    })
+
   }
 
 }
